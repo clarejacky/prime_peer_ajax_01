a4b2dcdcd6c5c5aa8ffd9493d9cd347104bbc3e9
clarejacky <clare.jacky@gmail.com>
2015-05-01 18:36:46 -0500

    ajax functionality

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
 var input;
+var avatar;
+var name;
+var link;
+
 function search(query){
 
     $.ajax ({
         type: 'GET',
         dataType: 'text',
         crossDomain: true,
-        url: 'https://api.github.com/users/'+ query,
+        url: 'https://api.github.com/users/'+ query+'?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d',
         complete: function() {
             console.log('ajax complete');
         },
-        success: function(data) {
+        success: function(data){
+            avatar = data.avatar_url;
+            name = data.login;
+            link = data.html_url;
             console.log(data);
-
+            $(".profileImg").attr("src", avatar);
+            $(".userName").text(name);
+            $(".pageLink").attr("href", link);
         }
     });
 
 }
 
-
 $(document).ready(function(){
-
-    search("tracyfuller");
+    //search("tracyfuller");
 //calling the api, CORS function, find the username, the link to their github page, profile image, public info
     //display an error message
 
-//on.click to bring in query terms for search function and call search function
-//$("#search-btn").on("click", function(event){
-//    event.preventDefault();
-//    input = $("#searchBox").val();
-//    console.log(input);
-//    search(input);
-//
-//
-//
-//
-//
-//});
+
+$("#search-btn").on("click", function(event){
+    event.preventDefault();
+    input = $("#searchBox").val();
+    console.log(input);
+    search(input);
+
+
+
+});
 
 //html_url
 //login - username
