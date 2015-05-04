e691ac4aa11e58b85d6ee862e18e32aa9f899152
clarejacky <clare.jacky@gmail.com>
2015-05-04 10:53:10 -0500

    populate to DOM

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 var input;
-var avatar;
 var name;
 var link;
+
 
 function search(query){
 
     $.ajax ({
         type: 'GET',
-        dataType: 'text',
+        dataType: 'json',
         crossDomain: true,
         url: 'https://api.github.com/users/'+ query+'?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d',
         complete: function() {
             console.log('ajax complete');
         },
         success: function(data){
-            avatar = data.avatar_url;
+
+            console.log(data);
+            var avatar = data.avatar_url;
+            console.log(data.avatar_url);
             name = data.login;
             link = data.html_url;
-            console.log(data);
             $(".profileImg").attr("src", avatar);
             $(".userName").text(name);
             $(".pageLink").attr("href", link);
